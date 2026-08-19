dfe8d93aea2e3ddfcad0af3f7fd180694f770c34
MattosCode12 <enzo_mattos@estudante.sesisenai.org.br>
2026-08-19 08:06:25 -0300

    ADD - Index PHP

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html> 
+
+<html lang="pt-br"> 
+    
+<head> 
+    <meta charset="UTF-8"> 
+    <title>Sistema Restaurante</title> 
+    <link rel="stylesheet" href="style.css"> 
+</head> 
+
+<body> 
+    <div class="container"> 
+        <h1>Sistema do Restaurante</h1> 
+        <a href="usuarios.php">Cadastrar Usuário</a> 
+        <a href="pratos.php">Gerenciar Pratos</a> 
+    </div> 
+</body> 
+</html>
