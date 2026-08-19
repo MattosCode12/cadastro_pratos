34aeb0c7e81b2a5ecb73f943cfbb1c06d2ad10e7
MattosCode12 <enzo_mattos@estudante.sesisenai.org.br>
2026-08-19 07:43:00 -0300

    ADD - Criação do Script do banco de dados

---
+++ banco.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE restaurante; 
+USE restaurante; 
+
+CREATE TABLE usuarios ( 
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    nome VARCHAR(100) NOT NULL, 
+    email VARCHAR(100) NOT NULL ); 
+    
+CREATE TABLE pratos ( 
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    nome VARCHAR(100) NOT NULL, 
+    descricao TEXT NOT NULL, 
+    preco DECIMAL(10,2) NOT NULL, 
+    categoria VARCHAR(100) NOT NULL, 
+    usuario_id INT NOT NULL, 
+    
+    FOREIGN KEY (usuario_id) 
+    REFERENCES usuarios(id) 
+    ON DELETE CASCADE 
+    );
